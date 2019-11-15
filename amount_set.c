c1eb0660550ae116409394d3f336450f093d3d17
aviveldan <aviveldan@gmail.com>
2019-11-15 17:14:51 +0200

    hELLO wORLD

--- amount_set.c
+++ amount_set.c
@@ -1,7 +1,25 @@
 //
 // Created by avive on 15/11/2019.
 //
+#include "amount_set.h"
+struct AmountSet_t{
+    ASElement first;
+    CopyASElement copyElement;
+    FreeASElement freeElement;
+    CompareASElements compareElements;
+};
 
-typedef struct AmountSet_t{
+AmountSet asCreate(CopyASElement copyElement,
+                   FreeASElement freeElement,
+                   CompareASElements compareElements){
+    if((CopyElement||freeElement||compareElements)==NULL){
+        return AS_NULL_ARGUMENT;
+    }
+    AmountSet result = NULL;
+    result = malloc(sizeof(result));
+    if(result==NULL){
+        return AS_OUT_OF_MEMORY;
+    }
 
-}; *AmountSet;
+    return result;
+}
