4677be892be2ad30824754f84e732b6776053e94
aviveldan <aviveldan@gmail.com>
2019-11-21 09:58:13 +0200

    added part2

---
+++ matamazom.c
@@ -0,0 +1,17 @@
+//
+// Created by avive on 21/11/2019.
+//
+#include "matamazom.h"
+#include "set.h"
+#include "list.h"
+struct Matamazom_t{};
+struct MatamazomProduct_t{
+    char* name;
+    unsigned int id;
+    MtmProductData data;
+    MatamazomAmountType type;
+};
+
+
+
+
