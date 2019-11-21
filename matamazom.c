baace5088ec8e981eda5e543a929499dabbfbac2
Lior <lioratias@campus.technion.ac.il>
2019-11-21 12:49:35 +0200

    try

--- matamazom.c
+++ matamazom.c
@@ -1,16 +1,35 @@
 //
 // Created by avive on 21/11/2019.
 //
+#include "stdlib.h"
 #include "matamazom.h"
 #include "set.h"
 #include "list.h"
-struct Matamazom_t{};
-struct MatamazomProduct_t{
+
+
+typedef struct MatamazomProduct_t {
     char* name;
     unsigned int id;
     MtmProductData data;
     MatamazomAmountType type;
+} *MatamazomProduct;
+
+typedef struct MatamazomOrder_t {
+
+} *MatamazomOrder;
+
+
+struct Matamazom_t{
+    MatamazomProduct first_product;
+    MatamazomOrder first_order;
+    int size;
+
 };
+
+Matamazom matamazomCreate() {
+    Matamazom matamazom = malloc(sizeof(*matamazom));
+    if
+}
 
 
 
